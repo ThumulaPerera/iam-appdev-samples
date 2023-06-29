f6d90aa2df79b983c35d2bfa1bb46b1a0d5a7da6
ThumulaPerera <42399179+ThumulaPerera@users.noreply.github.com>
2023-06-30 04:09:27 +0530

    Delete unnecessary files

--- fastify-svelte/svelte-web-app/src/hooks.server.js
+++
@@ -1,9 +0,0 @@
-// export async function handle({ event, resolve }) {
-
-//     const response = await resolve(event);
-
-//     console.log('from handle:' + event.locals.token);
-//     response.headers['set-cookie'] = `token=${event.locals.token || ''}; path=/; HttpOnly`
-
-//     return response;
-// }
