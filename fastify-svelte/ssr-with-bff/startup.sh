a62e7123ed60130a71c7fe4aedca815e5aeda0c3
ThumulaPerera <42399179+ThumulaPerera@users.noreply.github.com>
2023-07-07 09:32:01 +0530

    Improve startup script

--- fastify-svelte/ssr-with-bff/startup.sh
+++ fastify-svelte/ssr-with-bff/startup.sh
@@ -1,3 +1,9 @@
 #! /bin/sh 
-nohup node build &
-oauth2-proxy-v7.4.0.linux-amd64/oauth2-proxy --http-address 0.0.0.0:4180
+
+node build &
+
+oauth2-proxy-v7.4.0.linux-amd64/oauth2-proxy --http-address 0.0.0.0:4180 &
+
+wait -n
+
+exit $?
