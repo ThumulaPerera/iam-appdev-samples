3a6ea7f85b04c91380ad4a6a342fc758b3bc6fde
ThumulaPerera <42399179+ThumulaPerera@users.noreply.github.com>
2023-07-07 09:31:32 +0530

    Revert "Add distinction between internal and external url"
    This reverts commit 82f0c2072e8033c1ebcdad8829575e8f61284c7c.

--- fastify-svelte/ssr-with-bff/src/routes/+page.svelte
+++ fastify-svelte/ssr-with-bff/src/routes/+page.svelte
@@ -6,7 +6,6 @@
     let tokens = data.tokens;
     let user = data.user;
 
-    let proxyExternalUrl = env.PUBLIC_PROXY_EXTERNAL_URL;
     let proxyBaseUrl = env.PUBLIC_PROXY_BASE_URL;
     let proxySignInEndPoint = env.PUBLIC_PROXY_SIGN_IN_ENDPOINT;
     let proxySignOutEndPoint = env.PUBLIC_PROXY_SIGN_OUT_ENDPOINT;
@@ -42,7 +41,7 @@
     let unencodedSignOutRedirectUrl = createPlainUrl(asgardeoLogoutUrl, 
         { 
             id_token_hint: tokens?.id_token,
-            post_logout_redirect_uri: proxyExternalUrl + '/', 
+            post_logout_redirect_uri: proxyBaseUrl + '/', 
         }
     );
 
